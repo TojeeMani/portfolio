b134bdb551bc473d1ebd47169f2730df05fa987b
melvinjoy3 <melvinjoy8@gmail.com>
2025-04-08 22:18:50 +0530

    test deploy

--- profile/vite.config.js
+++ profile/vite.config.js
@@ -1,8 +1,17 @@
 // vite.config.js
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     hmr: {
       overlay: false, // Disable overlay if needed
     },
   },
-};
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
